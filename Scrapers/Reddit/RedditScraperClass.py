93675c56010c607a47011a54dcf713387d9205ac
Tim Rutjens <535094@fhict.nl>
2024-05-17 11:02:03 +0200

    added redditscraperclass skeleton based on uml

--- Scrapers/Reddit/RedditScraperClass.py
+++ Scrapers/Reddit/RedditScraperClass.py
@@ -5,4 +5,23 @@ Reddit scraper implementation
 #Imports
 
 class RedditScraper:
-    ...
+    
+    #Fields
+    query = str
+    selector = str
+    keywords = [str]
+
+    def __init__(self, query, selector):
+        self.query = query
+        self.selector = selector
+        pass
+
+    #Methods
+    async def subreddit_scrape(self, frequency, keywords) -> list:
+        ...
+
+    async def post_scrape(self, forums) -> dict:
+        ...
+
+    async def content_scrape(self, posts) -> dict:
+        ...
