5bbf92fcd08f153526d2ebce5e9dbd44ab6ad7cb
Tim Rutjens <535094@fhict.nl>
2024-05-23 11:17:48 +0200

    nu runt ie concurrently

--- testmain.py
+++ testmain.py
@@ -22,6 +22,33 @@ import unittest
 import logging
 import asyncio
 
+async def run_reddit(redditscraper, manager):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(args=['--start-maximized'], headless=False)
+
+        subreddits = await redditscraper.subreddit_scrape(browser)
+
+        posts = await redditscraper.post_scrape(forums=subreddits, browser=browser)
+
+        if posts:
+            comments = await redditscraper.content_scrape(posts=posts, browser=browser)
+            
+        await manager.insert_documents("redditposts", comments)
+
+async def run_twitter(twitterscraper, manager):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(args=['--start-maximized'], headless=False)
+
+        #keyword = 'EuroVision'
+
+        page = await twitterscraper.login_account(browser)
+        if page is None:
+            logging.error("Page is none")
+        links = await twitterscraper.link_gatherer(page)
+        twitterdata = await twitterscraper.scraper(browser, links)
+
+        await manager.insert_documents("tweets", twitterdata)
+
 async def main():
     username = None
     password = None
@@ -37,36 +64,14 @@ async def main():
     except FileNotFoundError as fe:
         logging.error(f"File not found: {fe}")    
 
-    twitterscraper = TwitterScraper(link_gather_account_username=username, link_gather_account_password=password)
+    twitterscraper = TwitterScraper(link_gather_account_username=username, link_gather_account_password=password, keyword=keyword)
     redditscraper = RedditScraper(query=keyword)
     manager = DBManager(db_name='scraped_data')
 
-    if twitterscraper:
-        async with async_playwright() as p:
-            browser = await p.chromium.launch(args=['--start-maximized'], headless=False)
-
-            #keyword = 'EuroVision'
-
-            page = await twitterscraper.login_account(browser)
-            if page is None:
-                logging.error("Page is none")
-            links = await twitterscraper.link_gatherer(page, keyword)
-            twitterdata = await twitterscraper.scraper(browser, links)
-
-            await manager.insert_documents("tweets", twitterdata)
-
-    if redditscraper:
-        async with async_playwright() as p:
-            browser = await p.chromium.launch(args=['--start-maximized'], headless=False)
-
-            subreddits = await redditscraper.subreddit_scrape(browser)
-
-            posts = await redditscraper.post_scrape(forums=subreddits, browser=browser)
-
-            if posts:
-                comments = await redditscraper.content_scrape(posts=posts, browser=browser)
-            
-            await manager.insert_documents("redditposts", comments)
+    await asyncio.gather(
+        run_twitter(twitterscraper=twitterscraper, manager=manager),
+        run_reddit(redditscraper=redditscraper, manager=manager)
+    )
 
 asyncio.run(main())
 
