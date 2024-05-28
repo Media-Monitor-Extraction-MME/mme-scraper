c191d45a40c09f75f7b06b4d1eab5f92cc489412
Tim Rutjens <535094@fhict.nl>
2024-05-28 10:49:40 +0200

    added error handling for twitter when tweet = None

--- Scrapers/Twitter/TwitterScraperClass.py
+++ Scrapers/Twitter/TwitterScraperClass.py
@@ -227,95 +227,114 @@ class TwitterScraper(IScraper):
 			for page, link in zip(pages, links[i:i+pages_per_context]):
 				async def scraping_logic(page, link):
 						print(link)
-						await page.goto(link)
+						try:
+							await page.goto(link)
 
-						'''
-						cookies_button = page.get_by_text("Refuse non-essential cookies")
-						if cookies_button:
-							await page.get_by_text("Refuse non-essential cookies").click()
-						else:
-							return
-						'''
-						
-						#Content
-						tweet_content = ''
+							'''
+							cookies_button = page.get_by_text("Refuse non-essential cookies")
+							if cookies_button:
+								await page.get_by_text("Refuse non-essential cookies").click()
+							else:
+								return
+							'''
+							
+							#Content
+							tweet_content = ''
+							try:
+								element = page.get_by_test_id('tweet')
 
-						element = page.get_by_test_id('tweet')
+								if element:
+									text_content = await element.text_content()
+									tweet_content += text_content
 
-						if element:
-							text_content = await element.text_content()
-							tweet_content += text_content
+									img_elements = await page.query_selector_all('img')
+									for img in img_elements:
+										alt_text = await img.get_attribute('alt')
+										if alt_text:
+											tweet_content += f" {alt_text} "
+								else:
+									tweet_content = 'Content element error'
+							except Exception as e:
+								print(f"Error loading tweet content {e}")
+								tweet_content = ''
 
-							img_elements = await page.query_selector_all('img')
-							for img in img_elements:
-								alt_text = await img.get_attribute('alt')
-								if alt_text:
-									tweet_content += f" {alt_text} "
-						else:
-							tweet_content = 'Content element error'
+							# Extract the date
+							date_pattern = r"\d{1,2}:\d{2} [ap]m · \d{1,2} \w+ \d{4}"
+							date = re.search(date_pattern, tweet_content)
+							tweet_date = date.group(0) if date else None
 
-						# Extract the date
-						date_pattern = r"\d{1,2}:\d{2} [ap]m · \d{1,2} \w+ \d{4}"
-						date = re.search(date_pattern, tweet_content)
-						tweet_date = date.group(0) if date else None
+							# Extract views, reposts, quotes, likes, and bookmarks
+							views_pattern = r"(\d[\d,.KkMm]*) Views"
+							reposts_pattern = r"(\d[\d,.KkMm]*) Reposts"
+							quotes_pattern = r"(\d[\d,.KkMm]*) Quotes"
+							likes_pattern = r"(\d[\d,.KkMm]*) Likes"
+							bookmarks_pattern = r"(\d[\d,.KkMm]*) Bookmarks"
 
-						# Extract views, reposts, quotes, likes, and bookmarks
-						views_pattern = r"(\d[\d,.KkMm]*) Views"
-						reposts_pattern = r"(\d[\d,.KkMm]*) Reposts"
-						quotes_pattern = r"(\d[\d,.KkMm]*) Quotes"
-						likes_pattern = r"(\d[\d,.KkMm]*) Likes"
-						bookmarks_pattern = r"(\d[\d,.KkMm]*) Bookmarks"
+							views = re.search(views_pattern, tweet_content)
+							reposts = re.search(reposts_pattern, tweet_content)
+							quotes = re.search(quotes_pattern, tweet_content)
+							likes = re.search(likes_pattern, tweet_content)
+							bookmarks = re.search(bookmarks_pattern, tweet_content)
 
-						views = re.search(views_pattern, tweet_content)
-						reposts = re.search(reposts_pattern, tweet_content)
-						quotes = re.search(quotes_pattern, tweet_content)
-						likes = re.search(likes_pattern, tweet_content)
-						bookmarks = re.search(bookmarks_pattern, tweet_content)
+							tweet_views = views.group(1) if views else None
+							tweet_reposts = reposts.group(1) if reposts else None
+							tweet_quotes = quotes.group(1) if quotes else None
+							tweet_likes = likes.group(1) if likes else None
+							tweet_bookmarks = bookmarks.group(1) if bookmarks else None
 
-						tweet_views = views.group(1) if views else None
-						tweet_reposts = reposts.group(1) if reposts else None
-						tweet_quotes = quotes.group(1) if quotes else None
-						tweet_likes = likes.group(1) if likes else None
-						tweet_bookmarks = bookmarks.group(1) if bookmarks else None
+							emoji_pattern = r"[\U00010000-\U0010ffff]"
 
-						emoji_pattern = r"[\U00010000-\U0010ffff]"
+							# Find the start index of the statistical data
+							data_pattern_start = re.search(date_pattern, tweet_content)
+							data_start_index = data_pattern_start.start() if data_pattern_start else None
 
-						# Find the start index of the statistical data
-						data_pattern_start = re.search(date_pattern, tweet_content)
-						data_start_index = data_pattern_start.start() if data_pattern_start else None
+							# Extract text content up to the point where statistical data starts
+							if data_start_index:
+								tweet_content_text = tweet_content[:data_start_index].strip()
+							else:
+								tweet_content_text = tweet_content
 
-						# Extract text content up to the point where statistical data starts
-						if data_start_index:
-							tweet_content_text = tweet_content[:data_start_index].strip()
-						else:
-							tweet_content_text = tweet_content
+							# Remove line breaks and multiple spaces
+							tweet_content_text = tweet_content_text.replace('\n', ' ')
+							tweet_content_text = ' '.join(tweet_content_text.split())
 
-						# Remove line breaks and multiple spaces
-						tweet_content_text = tweet_content_text.replace('\n', ' ')
-						tweet_content_text = ' '.join(tweet_content_text.split())
+							# Find and append the emojis
+							emoji = re.findall(emoji_pattern, tweet_content)
+							tweet_content_with_emoji = tweet_content_text + " " + ''.join(emoji).strip()
 
-						# Find and append the emojis
-						emoji = re.findall(emoji_pattern, tweet_content)
-						tweet_content_with_emoji = tweet_content_text + " " + ''.join(emoji).strip()
+							tweet_comments = ''
 
-						tweet_comments = ''
+							num = random.randint(0, 1000000)
+							hex_string = f'{num:024x}'
+							objectId = ObjectId(hex_string)
 
-						num = random.randint(0, 1000000)
-						hex_string = f'{num:024x}'
-						objectId = ObjectId(hex_string)
-
-						tweet = Tweet(_id=objectId,
-										link=link, 
-										content=tweet_content_with_emoji, 
-										date=tweet_date, 
-										likes=tweet_likes, 
-										reposts=tweet_reposts, 
-										quotes=tweet_quotes, 
-										bookmarks=tweet_bookmarks, 
-										views=tweet_views, 
-										comments=tweet_comments)
-						
-						return tweet.to_doc() 
+							tweet = Tweet(_id=objectId,
+											link=link, 
+											content=tweet_content_with_emoji, 
+											date=tweet_date, 
+											likes=tweet_likes, 
+											reposts=tweet_reposts, 
+											quotes=tweet_quotes, 
+											bookmarks=tweet_bookmarks, 
+											views=tweet_views, 
+											comments=tweet_comments)
+							
+							return tweet.to_doc() 
+						except Exception as e:
+							print(f"Error in scraping: {e}")
+							tweet = Tweet(_id=objectId,
+					 						link=link, 
+											content='', 
+											date='', 
+											likes='', 
+											reposts='', 
+											quotes='', 
+											bookmarks='', 
+											views='', 
+											comments=''
+					 						)
+						finally:
+							await page.close()
 
 				tasks.append(scraping_logic(page, link))
 
