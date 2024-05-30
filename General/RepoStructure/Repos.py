7a408ae741119097dc54eef4284c05f61b948447
Tim Rutjens <535094@fhict.nl>
2024-05-30 10:06:13 +0200

    fixed stupid import error

--- General/RepoStructure/Repos.py
+++ General/RepoStructure/Repos.py
@@ -6,7 +6,7 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from ..DB.InterfaceDBManager import IDBManager
-from IRepos import *
+from .IRepos import *
 
 from typing import Any
 
